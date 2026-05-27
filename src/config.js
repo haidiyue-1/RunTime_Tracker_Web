30464679d342f0bb67391cba26c96ffadf59427f
haidiyue-1 <2861511479@qq.com>
2026-05-27 10:23:13 +0800

    Update API_BASE URLs in config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const config = {
     dev: {
-        API_BASE: import.meta.env.VITE_API_BASE || 'https://incidents-starter-hence-chose.trycloudflare.com', // 默认开发环境
+        API_BASE: import.meta.env.VITE_API_BASE || 'https://supplements-offers-quotes-invitation.trycloudflare.com
+/api', // 默认开发环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/',
         SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
     },
     prod: {
-        API_BASE: import.meta.env.VITE_API_BASE || 'https://incidents-starter-hence-chose.trycloudflare.com/api', // 默认生产环境
+        API_BASE: import.meta.env.VITE_API_BASE || 'https://supplements-offers-quotes-invitation.trycloudflare.com
+/api', // 默认生产环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/',
         SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
     }
