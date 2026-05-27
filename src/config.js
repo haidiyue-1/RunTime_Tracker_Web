ceed73d6ee74896101916cc613544f03630caf87
haidiyue-1 <2861511479@qq.com>
2026-05-27 10:19:41 +0800

    Update API_BASE URLs for development and production

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
     dev: {
-        API_BASE: import.meta.env.VITE_API_BASE || 'https://scenic-baker-flexible-international.trycloudflare.com/api', // 默认开发环境
+        API_BASE: import.meta.env.VITE_API_BASE || 'https://incidents-starter-hence-chose.trycloudflare.com', // 默认开发环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/',
         SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
     },
     prod: {
-        API_BASE: import.meta.env.VITE_API_BASE || 'https://scenic-baker-flexible-international.trycloudflare.com/api', // 默认生产环境
+        API_BASE: import.meta.env.VITE_API_BASE || 'https://incidents-starter-hence-chose.trycloudflare.com/api', // 默认生产环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/',
         SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
     }
